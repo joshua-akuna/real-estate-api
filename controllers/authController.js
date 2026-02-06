8246a1c6deb51f8d2195fb7b5bc514be43fb9377
Akuna Joshua <akunajoshua@gmail.com>
2026-02-06 14:36:57 +0100

    user registration implemented

---
+++ controllers/authController.js
@@ -0,0 +1,55 @@
+const pool = require('../config/db');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'strict',
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+};
+
+// generate token
+function generateToken(id) {
+  return jwt.sign({ userId: id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+}
+
+const register = async (req, res) => {
+  try {
+    // destruct fields from req.boby
+    const { name, email, password, phone } = req.body;
+    // checks that all fields are available
+    if (!name || !email || !password || !phone) {
+      return res.status(400).json({ error: 'All fields are mandatory.' });
+    }
+    // checks if user exists
+    const userExists = await pool.query(
+      'SELECT * FROM users WHERE email = $1',
+      [email],
+    );
+    // return error if user already exists
+    if (userExists.rows.length > 0) {
+      return res.status(400).json({ error: 'Email already registered' });
+    }
+    // generate salt rounds
+    const salt = await bcrypt.genSalt(10);
+    // hash user password
+    const hash = await bcrypt.hash(password, salt);
+    // store user in database
+    const result = await pool.query(
+      'INSERT INTO users (name, email, password, phone) VALUES ($1, $2, $3, $4) RETURNING id, name, email, phone, created_at',
+      [name, email, hash, phone],
+    );
+    // gets new user
+    const user = result.rows[0];
+
+    res.cookie('token', generateToken(user.id), cookieOptions);
+
+    res.status(201).json({ user });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { register };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/db');
+const { register } = require('../controllers/authController');
 
-router.post('/register', (req, res) => {
-  try {
-    const { name, email, password, phone } = req.body;
-
-    if (!name || !email || !password || !phone) {
-      return res.status(400).json({ error: 'All fields are mandatory.' });
-    }
-    res.status(201).json({ name, email, password, phone });
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
-});
+router.post('/register', register);
 
 module.exports = router;
