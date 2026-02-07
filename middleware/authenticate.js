8d3de931fb08a0760b8b47784378b407874bee7b
Akuna Joshua <akunajoshua@gmail.com>
2026-02-07 16:49:13 +0100

    authenticate middleware refactored

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,19 +3,23 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authenticate = (req, res, next) => {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
   try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
   } catch (error) {
-    console.error(error);
-    return res.status(403).json({ error: 'Invalid token' });
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(403).json({ error: 'Invalid token' });
+    }
+    if (error.name === 'TokenExpiredError') {
+      return res.status(403).json({ error: 'Token expired' });
+    }
+    return res.status(500).json({ error: 'Authentication error' });
   }
 };
 
