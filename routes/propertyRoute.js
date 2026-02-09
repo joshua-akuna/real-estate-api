e8f9432fe2de49b3bbedc8b3c9d4226fd53bfc17
Akuna Joshua <akunajoshua@gmail.com>
2026-02-09 13:03:22 +0100

    propertyRoute refactored as country was added to properties table

--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -1,19 +1,59 @@
 const express = require('express');
-require('dotenv').config();
 const router = express.Router();
+const { body } = require('express-validator');
 
+const upload = require('../middleware/upload');
+const authenticate = require('../middleware/authenticate');
+const { createProperty } = require('../controllers/propertyControllers');
+
+// Validation rules for property creation
+const propertyValidationRules = [
+  body('title').trim().notEmpty().withMessage('Title is required'),
+  body('description').optional().trim(),
+  body('type').isIn(['sale', 'rent']).withMessage('Type must be sale or rent'),
+  body('property-type')
+    .trim()
+    .notEmpty()
+    .withMessage('Property type is required.'),
+  body('price')
+    .isFloat({ min: 0 })
+    .withMessage('Price must be a positive number'),
+  body('address').trim().notEmpty().withMessage('Address is required'),
+  body('city').trim().notEmpty().withMessage('City is required'),
+  body('state').trim().notEmpty().withMessage('State is required'),
+  body('country').trim().notEmpty().withMessage('Country is required'),
+  body('bedrooms')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Bedrooms must be positive integer'),
+  body('bathrooms')
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage('Bathrooms must be a positive number'),
+  body('area')
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage('Area must be a postive number'),
+];
+
+// public routes
 router.get('/', (req, res) => {
   res.json({ message: 'List of properties' });
-});
-
-router.post('/', (req, res) => {
-  res.json({ message: 'Property created' });
 });
 
 router.get('/:id', (req, res) => {
   const propertyId = req.params.id;
   res.json({ message: `Details of property with ID: ${propertyId}` });
 });
+
+// protected routes
+router.post(
+  '/',
+  authenticate,
+  upload.array('images', 10),
+  propertyValidationRules,
+  createProperty,
+);
 
 router.put('/:id', (req, res) => {
   const propertyId = req.params.id;
