311343cc9751f4774aad9a26807520ddec5c8d29
Akuna Joshua <akunajoshua@gmail.com>
2026-02-09 10:31:13 +0100

    upload filesize reduced to 7mb

--- middleware/upload.js
+++ middleware/upload.js
@@ -15,7 +15,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10MB
+  limits: { fileSize: 7 * 1024 * 1024 }, // Limit file size to 10MB
 });
 
 module.exports = upload;
