14f479c67bc4d7a5cc14e413c5cba745ffba7fc0
Akuna Joshua <akunajoshua@gmail.com>
2026-02-11 21:30:43 +0100

    added comments

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,13 +4,17 @@ require('dotenv').config();
 
 const authenticate = (req, res, next) => {
   try {
+    // Exract JWT token from HTTP-only cookies
     const token = req.cookies.token;
 
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
+    // verify token and extract userId
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // attach userId to request object for use in protected routes
     req.userId = decoded.userId;
+
     next();
   } catch (error) {
     if (error.name === 'JsonWebTokenError') {
