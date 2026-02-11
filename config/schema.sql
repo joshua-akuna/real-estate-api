f6280d7d155f4877952c980f28375617c3a6fe85
Akuna Joshua <akunajoshua@gmail.com>
2026-02-11 21:30:05 +0100

    removed NOT NULL from password column in users table

--- config/schema.sql
+++ config/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(255),
   phone VARCHAR(20),
   google_id VARCHAR(255) UNIQUE,
   profile_picture TEXT,
