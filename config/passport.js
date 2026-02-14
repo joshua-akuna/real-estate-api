2590d64ebf1ca2642b2faef56c84ad78c9892f07
Akuna Joshua <akunajoshua@gmail.com>
2026-02-14 12:04:39 +0100

    passport.js file refactored

--- config/passport.js
+++ config/passport.js
@@ -87,10 +87,7 @@ passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   try {
     // const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-    const result = await query(
-      'SELECT id, email, full_name, avatar_url, role FROM users WHERE id = $1',
-      [id],
-    );
+    const result = await query('SELECT * FROM users WHERE id = $1', [id]);
     if (result.rows.length === 0) {
       return done(null, false);
     }
