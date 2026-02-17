aa78654e6799683414f2f78e40670d445958636f
Akuna Joshua <akunajoshua@gmail.com>
2026-02-17 13:44:48 +0100

    uploadMultipleImages and deleteFromCloudinary functions implemented

--- service/cloudinaryService.js
+++ service/cloudinaryService.js
@@ -3,7 +3,6 @@ const streamifier = require('streamifier');
 
 // Upload a single file to cloudinary
 const uploadToCloudinary = (fileBuffer, folder = 'real-estate') => {
-  console.log('before upload');
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
@@ -25,7 +24,24 @@ const uploadToCloudinary = (fileBuffer, folder = 'real-estate') => {
     );
     streamifier.createReadStream(fileBuffer).pipe(uploadStream);
   });
-  console.log('after upload');
 };
 
-module.exports = { uploadToCloudinary };
+// Uploads multiple images
+const upLoadMultipleImgages = async (files, folder = 'real-estate') => {
+  const uploadPromises = files.map((file) =>
+    uploadToCloudinary(file.buffer, folder),
+  );
+  return await Promise.all(uploadPromises);
+};
+
+const deleteFromCloudinary = async (publicId) => {
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    return result;
+  } catch (error) {
+    console.error('Error deleting from Cloudinary:', error);
+    throw error;
+  }
+};
+
+module.exports = { uploadToCloudinary, upLoadMultipleImgages };
