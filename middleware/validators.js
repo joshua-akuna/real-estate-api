0171d150023bf8a9e3a2093563c90ac77a7f205c
Akuna Joshua <akunajoshua@gmail.com>
2026-02-19 16:50:21 +0100

    validators updated

--- middleware/validators.js
+++ middleware/validators.js
@@ -1,6 +1,6 @@
-const { body } = require('express-validator');
+const { body, param } = require('express-validator');
 
-// Validation rules
+// User validation rules
 const registerValidator = [
   body('email')
     .isEmail()
@@ -21,4 +21,88 @@ const loginValidator = [
   body('password').notEmpty().withMessage('Password is required'),
 ];
 
-module.exports = { registerValidator, loginValidator };
+// Property validation rules
+const creatPropertyValidators = [
+  body('title').trim().notEmpty().withMessage('Title is required'),
+  body('description').trim().notEmpty().withMessage('Description is required'),
+  body('property_type')
+    .trim()
+    .notEmpty()
+    .isIn(['house', 'apartment', 'condo', 'townhouse', 'land'])
+    .withMessage(
+      'Property type must be house, apartment, condo, townhouse or land',
+    ),
+  body('listing_type')
+    .isIn(['sale', 'rent'])
+    .withMessage('Listing type must be sale or rent'),
+  body('price')
+    .isFloat({ min: 0 })
+    .withMessage('Price must be a positive number'),
+  body('rental_period')
+    .optional()
+    .isIn(['day', 'week', 'month', 'year'])
+    .withMessage('Rental period must be day, week, month or year'),
+  body('bedrooms')
+    .notEmpty()
+    .withMessage('Number of bedrooms is required')
+    .isInt({ min: 0 })
+    .withMessage('Bedrooms must be positive integer'),
+  body('bathrooms')
+    .notEmpty()
+    .withMessage('Number of bathrooms is required')
+    .isFloat({ min: 0 })
+    .withMessage('Bathrooms must be a positive number'),
+  body('area_sqft')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Area must be a postive number'),
+  body('address').trim().notEmpty().withMessage('Address is required'),
+  body('city').trim().notEmpty().withMessage('City is required'),
+  body('state').trim().notEmpty().withMessage('State is required'),
+  body('zip_code').optional(),
+  body('country').trim().notEmpty().withMessage('Country is required'),
+  body('latitude').optional().isFloat().withMessage('Invalid latitude'),
+  body('longitude').optional().isFloat().withMessage('Invalid longitude'),
+];
+
+const updatePropertyValidator = [
+  param('id').isUUID().withMessage('Invalid property ID'),
+  body('title')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('Title cannot be empty'),
+  body('description')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('Description cannot be empty'),
+  body('property_type')
+    .optional()
+    .isIn(['house', 'apartment', 'condo', 'townhouse', 'land'])
+    .withMessage('Invalid property type'),
+  body('listing_type')
+    .optional()
+    .isIn(['sale', 'rent'])
+    .withMessage('Invalid listing type'),
+  body('price')
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage('Price must be a positive number'),
+  body('rental_period')
+    .optional()
+    .isIn(['day', 'week', 'month', 'year'])
+    .withMessage('Invalid rental period'),
+];
+
+const deletePropertyValidator = [
+  param('id').isUUID().withMessage('Invalid property ID'),
+];
+
+module.exports = {
+  registerValidator,
+  loginValidator,
+  creatPropertyValidators,
+  updatePropertyValidator,
+  deletePropertyValidator,
+};
